44efa01eeb48b822943c997217961aa31de9d650
fkingBug <fkingbug@gmail.com>
2021-08-10 16:04:10 +0300

    a

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ function App() {
     <div className="wrapper clear">
       <div className="overlay">
         <div className="drawer">
-          <h2 className="mb-30">Корзина</h2>
+          <h2 className="d-flex justify-between mb-30">
+            Корзина <img className="removeBtn cu-p" src="/img/btn-remove.svg" alt="Remove" />
+          </h2>
 
           <div className="items">
             <div className="cartItem d-flex align-center mb-20">
@@ -27,19 +29,23 @@ function App() {
               <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
             </div>
           </div>
-
-          <ul>
-            <li>
-              <span>Итого:</span>
-              <div></div>
-              <b>21 498 руб. </b>
-            </li>
-            <li>
-              <span>Налог 5%:</span>
-              <div></div>
-              <b>1074 руб. </b>
-            </li>
-          </ul>
+          <div className="cartTotalBlock">
+            <ul>
+              <li>
+                <span>Итого:</span>
+                <div></div>
+                <b>21 498 руб. </b>
+              </li>
+              <li>
+                <span>Налог 5%:</span>
+                <div></div>
+                <b>1074 руб. </b>
+              </li>
+            </ul>
+            <button className="greenButton">
+              Оформить заказ <img src="/img/arrow.svg" alt="Arrow" />
+            </button>
+          </div>
         </div>
       </div>
 
