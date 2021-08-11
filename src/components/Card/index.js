733a2ed1641188cefb19bfde290391a1615ef5b5
fkingBug <fkingbug@gmail.com>
2021-08-11 18:41:18 +0300

    third commit

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,17 +1,19 @@
-function Card() {
+import styles from './Card.module.scss'
+
+function Card(props) {
   return (
-    <div className="cart">
-      <div className="favorite">
+    <div className={styles.cart}>
+      <div className={styles.favorite}>
         <img src="/img/unliked.svg" alt="unliked" />
       </div>
-      <img width={133} height={112} src="/img/sneakers/1.jpg" alt="" />
-      <h5>Meжские кроссовки Nike Blazer Mid Suede</h5>
+      <img width={133} height={112} src={props.imageUrl} alt="" />
+      <h5>{props.title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column">
           <span>Цена :</span>
-          <b>12 999 руб.</b>
+          <b>{props.price} руб.</b>
         </div>
-        <button className="button">
+        <button className="button" onClick={props.onClick}>
           <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
         </button>
       </div>
